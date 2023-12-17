52e0a13a2d8411f562461ab2eebf1861739ee4b7
Zhanyl Baiguttieva <janabaiguttieva@gmail.com>
2023-12-18 00:26:54 +0600

    Added store.ts

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,10 @@
 import {configureStore} from "@reduxjs/toolkit";
+import {taskReducer} from "../containers/Task/taskSlice";
 
 
 export const  store = configureStore({
     reducer: {
-
+        task: taskReducer,
     },
 });
 export type RootState = ReturnType<typeof store.getState>;
