f1ff1a47c954b4ae5f6517c959dad1a5b46f7408
Zhanyl Baiguttieva <janabaiguttieva@gmail.com>
2023-12-19 15:31:17 +0600

    Added component Task and TakForm

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
-
 import './App.css';
+import Task from "./containers/Task/Task";
+import TaskForm from "./containers/TaskForm/TaskForm";
+import {TaskHeader} from "./types";
+import {useAppDispatch} from "./app/hooks";
+import {useEffect} from "react";
+import {createTask, fetchTask} from "./containers/Task/taskThunks";
 
 function App() {
+    const dispatch = useAppDispatch();
+    useEffect(() => {
+        dispatch(fetchTask());
+    }, [dispatch]);
+    const addNewTask = async (task: TaskHeader) => {
+        await dispatch(createTask(task));
+        await dispatch(fetchTask());
+    };
 
   return (
-      <div>
-          Hello
-      </div>
+    <div>
+      <Task/>
+      <TaskForm onSubmit={addNewTask}/>
+    </div>
   );
 }
 
