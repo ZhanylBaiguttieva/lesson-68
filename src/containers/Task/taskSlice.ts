926013a29984ffcf9035def6e2fa7535a38661dc
Zhanyl Baiguttieva <janabaiguttieva@gmail.com>
2023-12-19 15:29:48 +0600

    Added extraredusers

--- src/containers/Task/taskSlice.ts
+++ src/containers/Task/taskSlice.ts
@@ -1,24 +1,56 @@
 import {createSlice} from "@reduxjs/toolkit";
 
+import {Task} from "../../types";
+import {deleteTask, fetchTask} from "./taskThunks";
+import {RootState} from "../../app/store";
+
 interface TaskState {
-    header: string;
-    status: boolean;
+    data: Task[];
+    // status: boolean;
+    fetchLoading: boolean;
+    deleteLoading: false | string,
 }
 
 const initialState: TaskState = {
-    header: '',
-    status: false,
+    data: [],
+    // status: false,
+    fetchLoading: false,
+    deleteLoading: false,
 };
 
 export const taskSlice = createSlice({
-    name: 'task',
+    name: 'tasks',
     initialState,
     reducers: {
     },
+    extraReducers: (builder) => {
+        builder.addCase(fetchTask.pending,(state) => {
+            state.fetchLoading = true;
+        });
+        builder.addCase(fetchTask.fulfilled,(state, {payload: data}) => {
+            state.data = data;
+            state.fetchLoading = false;
+        });
+        builder.addCase(fetchTask.rejected,(state) => {
+            state.fetchLoading = false;
+        });
+
+        builder.addCase(deleteTask.pending, (state, {meta}) => {
+            state.deleteLoading = meta.arg;
+        });
+
+        builder.addCase(deleteTask.fulfilled, (state) => {
+            state.deleteLoading = false;
+        });
+
+        builder.addCase(deleteTask.rejected, (state) => {
+            state.deleteLoading = false;
+        });
+    }
 });
 
 export const taskReducer = taskSlice.reducer;
-// export const {
-//
-// } = taskSlice.actions;
+export const selectTask = (state: RootState) => state.task.data;
+export const selectFetchTaskLoading = (state: RootState) => state.task.fetchLoading;
+export const selectDeleteTaskLoading = (state: RootState) => state.task.deleteLoading;
 
